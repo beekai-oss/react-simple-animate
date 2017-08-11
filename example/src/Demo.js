65dedb164802aceb8f6ca20d658481b16a96b09f
Bill Luo <billluo@192-168-1-6.tpgi.com.au>
2017-08-12 08:41:09 +1000

    example code update and version update

--- example/src/Demo.js
+++ example/src/Demo.js
@@ -5,9 +5,20 @@ import Grid from 'material-ui/Grid';
 import DemoCode from './DemoCode';
 import Switch from 'material-ui/Switch';
 import DemoObject from './DemoObject';
+import Animate from 'react-simple-animate';
 import { FormControlLabel } from 'material-ui/Form';
 import { fields, selectOptions } from './DemoData';
 import './Demo.css';
+
+const startStyle = {
+  opacity: 0,
+};
+
+const endStyle = {
+  opacity: 1,
+};
+
+const delaySeconds = 0.1;
 
 export default class Demo extends React.Component {
   state = {
@@ -56,7 +67,7 @@ export default class Demo extends React.Component {
 
     return (
       <section className="demo-section">
-        <Grid container gutter={16}>
+        <Grid container spacing={16}>
           <Grid item xs={12} className="Demo-mode-selector">
             <FormControlLabel
               label="🤘 Easy Mode"
@@ -69,39 +80,52 @@ export default class Demo extends React.Component {
             />
           </Grid>
 
-          <Grid item xs={6}>
+          <Grid item xs={6} md={4}>
             {easyMode &&
-              <div className="demo-simple">
-                <label>Animation style: </label>
-                <select onChange={this.changeAnimateStyle}>
-                  {selectOptions.map(({ name }) => {
-                    return (
-                      <option value={name} key={name}>
-                        {name}
-                      </option>
-                    );
-                  })}
-                </select>
-              </div>}
+              <Animate startAnimation {...{ startStyle, endStyle }}>
+                <div className="demo-simple">
+                  <label>Animation style: </label>
+                  <select onChange={this.changeAnimateStyle}>
+                    {selectOptions.map(({ name }) => {
+                      return (
+                        <option value={name} key={name}>
+                          {name}
+                        </option>
+                      );
+                    })}
+                  </select>
+                </div>
+              </Animate>}
 
             {!easyMode &&
-              fields.map(field => {
-                return (
-                  <Grid item xs={12} key={field.value}>
-                    <TextField
-                      className="demo-section-input"
-                      rowsMax="4"
-                      onChange={e =>
-                        this.handleChange(field.value, e.target.value)}
-                      value={this.state[field.value]}
-                      label={field.label}
-                    />
-                  </Grid>
-                );
-              })}
+              <Animate
+                startAnimation
+                {...{ startStyle, endStyle, delaySeconds }}
+              >
+                {fields.map((field, i) => {
+                  return (
+                    <Grid item xs={12} key={field.value}>
+                      <TextField
+                        className="demo-section-input"
+                        rowsMax="4"
+                        fullWidth
+                        onChange={e =>
+                          this.handleChange(field.value, e.target.value)}
+                        value={this.state[field.value]}
+                        label={field.label}
+                        multiline={i < 2}
+                        helperText={
+                          i < 2 &&
+                          `Note: value is json format, eg: {"key": "value"}`
+                        }
+                      />
+                    </Grid>
+                  );
+                })}
+              </Animate>}
           </Grid>
 
-          <Grid item xs={6}>
+          <Grid item xs={6} md={8}>
             <DemoObject {...this.state} />
           </Grid>
 

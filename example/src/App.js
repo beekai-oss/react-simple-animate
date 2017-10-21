2572d281062663adfe0ecc2816d68b2330865e8e
Bill Luo <billluo@192-168-1-6.tpgi.com.au>
2017-10-22 09:44:36 +1100

    demo add blog and github button

--- example/src/App.js
+++ example/src/App.js
@@ -3,16 +3,25 @@ import Toolbar from 'material-ui/Toolbar';
 import Typography from 'material-ui/Typography';
 import React from 'react';
 import Demo from './Demo';
+import Button from 'material-ui/Button';
 import './App.css';
 
 export default function App() {
   return (
-    <div>
+    <div className="App-bar">
       <AppBar position="static">
         <Toolbar>
           <Typography type="title" color="inherit">
             React Simple Animate
           </Typography>
+
+          <a href="https://medium.com/jsdownunder/react-ui-animation-made-simple-c2ff98056659">
+            <Button color="contrast">Blog</Button>
+          </a>
+
+          <a href="https://github.com/bluebill1049/react-simple-animate">
+            <Button color="contrast">Github</Button>
+          </a>
         </Toolbar>
       </AppBar>
       <Demo />

38ad7b041cf7b62076394ec98eac98e6edf4b63a
bill <bluebill1049@hotmail.com>
2018-11-12 12:23:14 +1100

    build with rollup.js

--- src/animate.js
+++ src/animate.js
@@ -43,7 +43,7 @@ export type State = {
 };
 
 export class AnimateChild extends React.PureComponent<Props, State> {
-  static displayName = 'ReactSimpleAnimate';
+  static displayName = 'Animate';
 
   static defaultProps = {
     durationSeconds: 0.3,

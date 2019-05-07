27e15f57a4d1a2d526cfc07828e3b7e61a665d5a
bill <bluebill1049@hotmail.com>
2019-05-07 15:04:54 +1000

    fix bug with useAnimateKeyframes

--- src/useAnimateKeyframes.js
+++ src/useAnimateKeyframes.js
@@ -1,6 +1,5 @@
 // @flow
-// $FlowIgnoreLine
-import { useState, useEffect } from 'react';
+import { useState, useEffect, useRef } from 'react';
 import createRandomName from './utils/createRandomName';
 import createTag from './style/createTag';
 import type { AnimateKeyframesProps } from './animateKeyframes';
@@ -21,9 +20,9 @@ export default function useAnimateKeyframes(props: UseAnimateKeyframesProps) {
     playState = 'running',
     keyframes,
   } = props;
-
-  const [animateProps, setPlay] = useState(props);
-
+  const animationNameRef = useRef('');
+  const styleTagRef = useRef(null);
+  const [{ play }, setPlay] = useState(props);
   const playFunction = (playValue: boolean) => {
     setPlay({
       ...props,
@@ -31,26 +30,29 @@ export default function useAnimateKeyframes(props: UseAnimateKeyframesProps) {
     });
   };
 
-  const { play, animationName } = animateProps;
+  useEffect(
+    () => {
+      if (!animationNameRef.current) {
+        const name = createRandomName();
+        const { styleTag } = createTag({ animationName: name, keyframes });
+        styleTagRef.current = styleTag;
+        animationNameRef.current = name;
+      }
 
-  useEffect(() => {
-    const name = createRandomName();
-    const { styleTag } = createTag({ animationName: name, keyframes });
-    const localStyleTag: any = styleTag;
-
-    setPlay({
-      ...props,
-      animationName: name,
-    });
-
-    return () => {
-      deleteRules(localStyleTag.sheet, name);
-    };
-  }, []);
+      return () => {
+        if (styleTagRef.current && animationNameRef.current) {
+          // $FlowIgnoreLine
+          deleteRules(styleTagRef.current.sheet, animationNameRef.current);
+        }
+      };
+    },
+    [play],
+  );
 
   const style = play
     ? {
-        animation: `${durationSeconds}s ${easeType} ${delaySeconds}s ${iterationCount} ${direction} ${fillMode} ${playState} ${animationName}`,
+        animation: `${durationSeconds}s ${easeType} ${delaySeconds}s ${iterationCount} ${direction} ${fillMode} ${playState} ${animationNameRef.current ||
+          ''}`,
       }
     : null;
 
@@ -58,7 +60,6 @@ export default function useAnimateKeyframes(props: UseAnimateKeyframesProps) {
     {
       style,
       play,
-      animationName,
     },
     (playValue: boolean) => playFunction(playValue),
   ];

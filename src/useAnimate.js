9d8079e69a99810de7aca9cd90e04ce911f35898
Beier Luo <bluebill1049@hotmail.com>
2019-05-12 22:08:29 +1000

    fixing useAnimate

--- src/useAnimate.js
+++ src/useAnimate.js
@@ -4,25 +4,28 @@ import type { Props } from './animate';
 import msToSec from './utils/msToSec';
 
 export default function useAnimate(props: Props) {
-  const { play, start, end, complete, onComplete, delay = 0, duration = 0.3, easeType = 'linear' } = props;
-  const [style, setStyle] = useState(start);
+  const { start, end, complete, onComplete, delay = 0, duration = 0.3, easeType = 'linear' } = props;
+  const transition = `all ${duration}s ${easeType} ${delay}s`;
+  const [style, setStyle] = useState({ ...start, transition });
   const onCompleteTimeRef = useRef(null);
 
   useEffect(() => {
+    return () => onCompleteTimeRef.current && clearTimeout(onCompleteTimeRef.current);
+  }, [duration, easeType, delay, onComplete, start, end, complete]);
+
+  const play = isPlaying => {
     setStyle({
-      ...(play.play ? end : start),
-      transition: `all ${duration}s ${easeType} ${delay}s`,
+      ...(isPlaying ? end : start),
+      transition,
     });
 
-    if (play && (complete || onComplete)) {
+    if (isPlaying && (complete || onComplete)) {
       onCompleteTimeRef.current = setTimeout(() => {
         complete && setStyle(complete);
         onComplete && onComplete();
       }, msToSec(delay + parseFloat(duration)));
     }
-
-    return () => onCompleteTimeRef.current && clearTimeout(onCompleteTimeRef.current);
-  }, [play, duration, easeType, delay, onComplete, start, end, complete]);
+  };
 
   return {
     style,

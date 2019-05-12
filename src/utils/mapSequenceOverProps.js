fd62af377c233c68114cbc27c8c637ff3fec912c
Beier Luo <bluebill1049@hotmail.com>
2019-05-12 18:53:07 +1000

    delete unused files

--- src/utils/mapSequenceOverProps.js
+++
@@ -1,9 +0,0 @@
-// @flow
-import type { Props } from '../animate';
-
-export default (props: Props, id?: string | number) => {
-  const { animationStates, ...restProps } = props;
-  if (!id || !animationStates || !animationStates[id]) return props;
-  const stateCopy = { ...animationStates[id] };
-  return { ...stateCopy, ...restProps };
-};

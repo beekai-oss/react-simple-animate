d850c414792df67e559d4b9f1b11bc3f4c255f87
bill <bluebill1049@hotmail.com>
2019-05-14 15:39:54 +1000

    unit test for AnimateKeyFrames

---
+++ src/useAnimateKeyframes.test.tsx
@@ -0,0 +1,44 @@
+import * as React from 'react';
+import useAnimateKeyframes from './useAnimateKeyframes';
+import { mount } from 'enzyme';
+import { act } from 'react-dom/test-utils';
+
+jest.mock('./utils/createRandomName', () => ({
+  default: () => 'test',
+}));
+
+let UseAnimate;
+
+describe('useAnimate', () => {
+  let componentStyle;
+
+  const TestHook = ({ callback }) => {
+    const { style } = callback();
+    componentStyle = style;
+    return <div style={style}>errors</div>;
+  };
+
+  const TestComponent = callback => {
+    mount(<TestHook callback={callback} />);
+  };
+
+  beforeEach(() => {
+    TestComponent(() => {
+      UseAnimate = useAnimateKeyframes({ keyframes: ['opacity: 0', 'opacity: 1'] });
+      return UseAnimate;
+    });
+
+    jest.resetAllMocks();
+  });
+
+  it('should toggle style correctly', () => {
+    act(() => {
+      expect(UseAnimate.play(true)).toBeUndefined();
+      expect(componentStyle).toEqual(null);
+    });
+
+    expect(componentStyle).toEqual({
+      animation: '0.3s linear 0s 1 normal none running test',
+    });
+  });
+});

5490952ff48a589f725c640fc06ba24bb27dd2c9
bill <bluebill1049@hotmail.com>
2019-05-16 17:39:58 +1000

    fix createStyle with string chceck

--- src/logic/createStyle.ts
+++ src/logic/createStyle.ts
@@ -9,13 +9,15 @@ export default function createStyle({
 }): string {
   const animationLength = keyframes.length;
 
-  return `${keyframes.reduce((previous, keyframe, currentIndex): string => {
-    if (typeof keyframe === 'string') {
-      return `${previous} ${
-        animationLength === 2 ? currentIndex * 100 : parseFloat((100 / (animationLength - 1)).toFixed(2)) * currentIndex
-      }% {${keyframe}}`;
-    }
-
-    return `${previous} ${Object.keys(keyframe)[0]}% {${Object.values(keyframe)[0]}}`;
-  }, `@keyframes ${animationName} {`)}}`;
+  return `${keyframes.reduce(
+    (previous, keyframe, currentIndex): string =>
+      typeof keyframe === 'string'
+        ? `${previous} ${
+            animationLength === 2
+              ? currentIndex * 100
+              : parseFloat((100 / (animationLength - 1)).toFixed(2)) * currentIndex
+          }% {${keyframe}}`
+        : `${previous} ${Object.keys(keyframe)[0]}% {${Object.values(keyframe)[0]}}`,
+    `@keyframes ${animationName} {`,
+  )}}`;
 }

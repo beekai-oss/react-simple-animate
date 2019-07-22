d830c9f2928911a11adc1a0d14947508f99e92f7
Sheldon Liu <pangpangilily@gmail.com>
2019-07-22 17:39:42 +1000

    Remove comment and update readme

--- src/logic/createStyle.ts
+++ src/logic/createStyle.ts
@@ -4,22 +4,27 @@ import camelCaseToDash from '../utils/camelCaseToDash';
 const generateKeyframes = keyframes => {
   const animationLength = keyframes.length;
   return keyframes.reduce((previous, keyframe, currentIndex) => {
-    const keyframePersentage = animationLength === 2
-      ? currentIndex * 100
-      : parseFloat((100 / (animationLength - 1)).toFixed(2)) * currentIndex;
-      // ['opacity: 0', 'opacity: 1']
-      if (typeof keyframe === 'string') {
-        return `${previous} ${keyframePersentage}% {${keyframe}}`;
-      }
-      const keys = Object.keys(keyframe);
-      // [{opacity: 0}, {opacity: 1}]
-      if (keys.length && isNaN(+keys[0])) {
-        const keyframeContent = keys.reduce((acc, key) => `${acc} ${camelCaseToDash(key)}: ${keyframe[key]};`, '');
-        return `${previous} ${keyframePersentage}% {${keyframeContent}}`;
-      }
-      // [{0: 'opacity: 0'}, {50: 'opacity: 0.5'}, {100: 'opacity: 1' }];
-      return `${previous} ${Object.keys(keyframe)[0]}% {${Object.values(keyframe)[0]}}`;
-    }, '');
+    const keyframePersentage =
+      animationLength === 2
+        ? currentIndex * 100
+        : parseFloat((100 / (animationLength - 1)).toFixed(2)) * currentIndex;
+
+    if (typeof keyframe === 'string') {
+      return `${previous} ${keyframePersentage}% {${keyframe}}`;
+    }
+    const keys = Object.keys(keyframe);
+
+    if (keys.length && isNaN(+keys[0])) {
+      const keyframeContent = keys.reduce(
+        (acc, key) => `${acc} ${camelCaseToDash(key)}: ${keyframe[key]};`,
+        '',
+      );
+      return `${previous} ${keyframePersentage}% {${keyframeContent}}`;
+    }
+    return `${previous} ${Object.keys(keyframe)[0]}% {${
+      Object.values(keyframe)[0]
+    }}`;
+  }, '');
 };
 
 export default function createStyle({
@@ -30,4 +35,4 @@ export default function createStyle({
   animationName: string;
 }): string {
   return `@keyframes ${animationName} {${generateKeyframes(keyframes)}}`;
-};
+}

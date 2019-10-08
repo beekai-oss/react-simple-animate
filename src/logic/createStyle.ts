36b143ff404e2ae430c31843c69582c2c7fd277f
Tiaan <tiaanduplessis@hotmail.com>
2019-10-08 12:17:45 +0200

    Spelling mistake fix (#64)
    * [ImgBot] Optimize images
    
    /logo.png -- 109.91kb -> 37.57kb (65.82%)
    
    Signed-off-by: ImgBotApp <ImgBotHelp@gmail.com>
    
    * Fix spelling mistake

--- src/logic/createStyle.ts
+++ src/logic/createStyle.ts
@@ -4,13 +4,13 @@ import camelCaseToDash from '../utils/camelCaseToDash';
 const generateKeyframes = keyframes => {
   const animationLength = keyframes.length;
   return keyframes.reduce((previous, keyframe, currentIndex) => {
-    const keyframePersentage =
+    const keyframePercentage =
       animationLength === 2
         ? currentIndex * 100
         : parseFloat((100 / (animationLength - 1)).toFixed(2)) * currentIndex;
 
     if (typeof keyframe === 'string') {
-      return `${previous} ${keyframePersentage}% {${keyframe}}`;
+      return `${previous} ${keyframePercentage}% {${keyframe}}`;
     }
     const keys = Object.keys(keyframe);
 
@@ -19,7 +19,7 @@ const generateKeyframes = keyframes => {
         (acc, key) => `${acc} ${camelCaseToDash(key)}: ${keyframe[key]};`,
         '',
       );
-      return `${previous} ${keyframePersentage}% {${keyframeContent}}`;
+      return `${previous} ${keyframePercentage}% {${keyframeContent}}`;
     }
     return `${previous} ${Object.keys(keyframe)[0]}% {${
       Object.values(keyframe)[0]

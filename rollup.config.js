25f085ad47c932e0663e0c27dd528a780782d646
Gabrijel Skoro <gabrijel.skoro@gmail.com>
2021-02-19 18:30:48 +0100

    add support for IE11

--- rollup.config.js
+++ rollup.config.js
@@ -1,9 +1,14 @@
 import typescript from 'rollup-plugin-typescript2';
+import { babel } from '@rollup/plugin-babel';
 
 export default {
   input: 'src/index.ts',
   plugins: [
     typescript(),
+    babel({
+      babelHelpers: 'bundled',
+      extensions: ['.ts', '.tsx']
+    })
   ],
   external: ['react'],
   output: [

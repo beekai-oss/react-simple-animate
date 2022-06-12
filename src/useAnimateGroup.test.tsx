48f1a4f094d65e44128a5352014fd6c03a60e424
Ali.Garajian <ali.garajian@snapp.cab>
2022-06-12 17:48:18 +0430

    fix(useAnimateGroup): set the value of isPlaying correctly when calling play

--- src/useAnimateGroup.test.tsx
+++ src/useAnimateGroup.test.tsx
@@ -1,7 +1,7 @@
 import * as React from 'react';
 import { mount } from 'enzyme';
 import useAnimateGroup from './useAnimateGroup';
-import { act } from 'react-test-renderer';
+import { act } from 'react-dom/test-utils';
 
 jest.mock('./utils/createRandomName', () => ({
   default: () => 'test',
@@ -49,17 +49,55 @@ describe('useAnimateGroup', () => {
       undefined,
     ]);
 
+    const forwardStyles = [
+      { opacity: 1, transition: 'all 0.3s linear 0s' },
+      { animation: '0.3s linear 0.3s 1 normal none running test' },
+      { opacity: 1, transition: 'all 0.3s linear 0.6s' },
+      {
+        animation: '0.3s linear 0.8999999999999999s 1 normal none running test',
+      },
+    ];
+    const reverseStyles = [
+      { opacity: 0, transition: 'all 0.3s linear 0.8999999999999999s' },
+      { animation: '0.3s linear 0.6s 1 normal none running test' },
+      { opacity: 0, transition: 'all 0.3s linear 0.3s' },
+      {
+        animation: '0.3s linear 0s 1 normal none running test',
+      },
+    ];
+
     act(() => {
       UseAnimateGroup.play(true);
-      expect(componentStyle).toEqual([
-        { opacity: 1, transition: 'all 0.3s linear 0s' },
-        { animation: '0.3s linear 0.3s 1 normal none running test' },
-        { opacity: 1, transition: 'all 0.3s linear 0.6s' },
-        {
-          animation:
-            '0.3s linear 0.8999999999999999s 1 normal none running test',
-        },
-      ]);
     });
+    expect(componentStyle).toEqual(forwardStyles);
+
+    act(() => {
+      UseAnimateGroup.play(false);
+    });
+    expect(componentStyle).toEqual(reverseStyles);
+  });
+
+  it('should update isPlaying after calling play', () => {
+    TestComponent(() => {
+      UseAnimateGroup = useAnimateGroup({
+        sequences: [],
+      });
+      return UseAnimateGroup;
+    });
+
+    act(() => {
+      UseAnimateGroup.play(true);
+    });
+    expect(UseAnimateGroup.isPlaying).toEqual(true);
+
+    act(() => {
+      UseAnimateGroup.play(false);
+    });
+    expect(UseAnimateGroup.isPlaying).toEqual(false);
+
+    act(() => {
+      UseAnimateGroup.play(true);
+    });
+    expect(UseAnimateGroup.isPlaying).toEqual(true);
   });
 });
